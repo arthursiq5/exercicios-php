a86a50c1bb079ece4e6ae9d519bc5a69e6f3f29b
Arthur Siqueira <arthur.siq5@gmail.com>
2021-07-05 13:26:18 -0300

    Adicionado teste de módulo

--- test/Basico/OperadoresTest.php
+++ test/Basico/OperadoresTest.php
@@ -10,9 +10,12 @@ class OperadoresTest extends TestCase
     {
         $num1 = 8;
         $num2 = 6;
+        
         $this->assertEquals(14, $num1 + $num2);
         $this->assertEquals(2, $num1 - $num2);
         $this->assertEquals(48, $num1 * $num2);
         $this->assertEquals(1.33, round($num1 / $num2, 2));
+
+        $this->assertEquals(2, $num1 % $num2);
     }
 }
