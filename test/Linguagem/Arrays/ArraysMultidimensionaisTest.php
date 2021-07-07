f51c66eb69c6ef4b26e3b7f0117177860112f36a
arthursiq5 <arthur.siq5@gmail.com>
2021-07-07 17:25:58 -0300

    Adicionados testes com array multidimensional

---
+++ test/Linguagem/Arrays/ArraysMultidimensionaisTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Test\Arrays;
+
+use PHPUnit\Framework\TestCase;
+
+class ArraysMultidimensionaisTest extends TestCase
+{
+    public function testPrimeiroArrayMultidimensional(): void
+    {
+        $people = [
+            'online' => ['David', 'Amy'],
+            'offline' => ['John', 'Rob', 'Jack'],
+            'away' => ['Arthur', 'Daniel']
+        ];
+
+        $this->assertIsArray($people);
+        $this->assertIsArray($people['online']);
+        $this->assertIsArray($people['offline']);
+        $this->assertIsArray($people['away']);
+
+        $this->assertEquals(['David', 'Amy'], $people['online']);
+        $this->assertEquals(['John', 'Rob', 'Jack'], $people['offline']);
+        $this->assertEquals(['Arthur', 'Daniel'], $people['away']);
+    }
+}
