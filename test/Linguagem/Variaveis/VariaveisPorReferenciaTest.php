05831c147e78b47346708257eab0c51f688005e5
arthursiq5 <arthur.siq5@gmail.com>
2021-07-07 17:13:04 -0300

    Adicionados mais exemplos de uso de variáveis por referência

--- test/Linguagem/Variaveis/VariaveisPorReferenciaTest.php
+++ test/Linguagem/Variaveis/VariaveisPorReferenciaTest.php
@@ -10,17 +10,26 @@ class VariaveisPorReferenciaTest extends TestCase
     {
         $message1 = '[MESSAGE] mensagem 1';
         $message2 = '[MESSAGE] mensagem 2';
+        $message3 = 3.14;
 
         $a = $message1;
         $b = &$a; // $b é uma referência de $a
 
+        $this->assertIsString($a);
         $this->assertEquals($message1, $a);
         $this->assertEquals($message1, $b);
+        $this->assertSame($a, $b);
 
         $b = $message2;
-
+        $this->assertIsString($a);
         $this->assertEquals($message2, $a);
         $this->assertEquals($message2, $b);
+        $this->assertSame($a, $b);
 
+        $a = $message3;
+        $this->assertIsFloat($a);
+        $this->assertEquals($message3, $a);
+        $this->assertEquals($message3, $b);
+        $this->assertSame($a, $b);
     }
 }
