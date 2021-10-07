448a979e39e0b6e6b471c8019d14f33f92b21123
arthursiq5 <arthur.siq5@gmail.com>
2021-10-07 13:17:37 -0300

    Adicionado arquivo com exemplos da sintaxe da linguagem

---
+++ src/Basico/Sintaxe.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Syntax</title>
+</head>
+<body>
+    <?php
+        echo 'Hello World!<hr/>';
+        // comment
+        # comment
+        /*
+        multi-line comment
+        */
+        $x = 10 /* + 5 */ + 20; // resultado: 30
+        echo $x;
+        echo '<hr/>';
+    ?>
+</body>
+</html>
