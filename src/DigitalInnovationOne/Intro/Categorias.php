b213f3dcc474d7ed9c2dac727dcbd5942f571f4e
arthursiq5 <arthur.siq5@gmail.com>
2021-12-02 22:06:45 -0300

    Iniciado desenvolvimento do sistema

---
+++ src/DigitalInnovationOne/Intro/Categorias.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\DigitalInnovationOne\Intro;
+
+class Categorias
+{
+    public static $categorias = [
+        'infantil',
+        'adolescente',
+        'adulto'
+    ];
+}
